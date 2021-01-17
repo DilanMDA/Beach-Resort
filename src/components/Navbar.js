6be12b13ca9355c15e6c4044d4deb8a1ce2379ce
DilanMDA <akiladilan04@gmail.com>
2021-01-17 22:13:56 +0530

    navbar component added

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+//class base components bcz we do toggling
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                 hello from navbar
+            </div>
+        )
+    }
+}
