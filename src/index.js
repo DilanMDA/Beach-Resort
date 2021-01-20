5f87d747eb517fad0d88aaf83717c4a695540f9f
DilanMDA <akiladilan04@gmail.com>
2021-01-20 15:09:49 +0530

    experimental context added[added featured component and Context API]

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {
-  BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RoomProvider} from './context/context';
  
-
 ReactDOM.render(
+  <RoomProvider>
+
   < Router>
     <App />
-   
-  </ Router>,
+  </ Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
