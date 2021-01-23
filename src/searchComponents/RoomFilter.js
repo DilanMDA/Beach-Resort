5a74f316cf5d2e9158b15629a9d07a103aca6df1
DilanMDA <akiladilan04@gmail.com>
2021-01-24 01:34:19 +0530

    RoomFilter Title added

--- src/searchComponents/RoomFilter.js
+++ src/searchComponents/RoomFilter.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import {useContext} from 'react';
 import {RoomContext} from '../context/context';
+import Title from '../components/Title';
 export default function RoomFilter() {
     const context = useContext(RoomContext);
-    console.log(context);
-    
+    console.log(context); 
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
     return (
-        <div>
-            hello from rooms filter
-        </div>
-    )
+        <section className = 'filter-container'>
+           <Title title='search rooms'/>
+        </section>
+    );
 }
